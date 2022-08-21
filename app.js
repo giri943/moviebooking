4abf593a4a3c98add6ef88e53a5878697e520b5d
Girish Soman <girish.soman@schbang.com>
2022-08-21 11:31:14 +0530

    feat:server connection setup completed

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const colors = require('colors')
+
+const { mongoose } = require("./db/mongoose");
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.listen(port, (err) => {
+    if (err) {
+      return console.log("Error running the app");
+    }
+    console.log(`App is running on port ${port}`);
+  })
